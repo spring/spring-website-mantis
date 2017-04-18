2042f86e1d3092ba3df92cedb2a5a85c4203b4fe
abma <spring@abma.de>
2017-04-19 01:55:04 +0200

    initial commit

---
+++ springpw_example.php
@@ -0,0 +1,20 @@
+<?php
+
+// Copy this file to springpw.php and update these variables
+// If db information changes, remember to change /wwwroot/piwik/config/config.ini.php as well
+
+$spring_dbhost = 'localhost';
+$spring_dbuser = 'spring';
+$spring_dbpass = 'some_pass';
+$spring_dbname = 'spring';
+
+// Mantis specific
+
+# String used to generate the confirm_hash for the 'lost password' feature and captcha code for 'signup'
+# ATTENTION: CHANGE IT TO WHATEVER VALUE YOU PREFER
+$g_password_confirm_hash_magic_string = 'blowfish';
+
+// Mediawiki specific
+
+$wgProxyKey = "646c9f400fba3196b77c3822de9aa048e7891dcd216d2ff9734aab9a0b0e9365";
+
